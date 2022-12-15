7d45b78fb4c75643c916540a0a5e1cabf3231dfb
foxido <git@forestfox.tk>
2022-12-15 19:18:21 +0300

    Fix incorrect condition in ERR_CASE & Fix memory leak if  ast file is incorrect

--- backend/main.cpp
+++ backend/main.cpp
@@ -10,6 +10,7 @@
 {                                                   \
     if (cond) {                                     \
         fprintf (stderr, fmt "\n", ##__VA_ARGS__);  \
+        tree::del_node (ast);                       \
         return ERROR;                               \
     }                                               \
 }
@@ -18,22 +19,24 @@
 
 int main (int argc, const char *argv[])
 {
-    ERR_CASE (argc != 3 || strcmp (argv[1], "-h"), "Usage: ./back <input ast file> <output asm file>");
+    tree::node_t *ast = nullptr;
 
+    ERR_CASE (argc != 3 || (strcmp (argv[1], "-h") == 0), "Usage: ./back <input ast file> <output asm file>");
+    
     const file_t src = open_ro_file (argv[1]);
     ERR_CASE (src.content == nullptr, "Failed to open file %s", argv[1]);
 
     const char *tree_section = src.content;
     while (*tree_section != '{') tree_section++;
 
-    tree::node_t *ast = tree::load_tree (tree_section);
+    ast = tree::load_tree (tree_section);
     ERR_CASE (ast == nullptr, "Failed to load AST tree");
     unmap_ro_file (src);
 
     FILE *output_file = fopen (argv[2], "w");
     ERR_CASE (output_file == nullptr, "Failed to open file %s", argv[2]);
 
-    ERR_CASE (compiler::compile (ast, output_file), "Failed to compile, see logs");
+    ERR_CASE (!compiler::compile (ast, output_file), "Failed to compile, see logs");
 
     fclose (output_file);
     tree::del_node (ast);
