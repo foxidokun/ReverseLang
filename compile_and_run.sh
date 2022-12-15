3c48ce90713d0f08597d80d684a92ab8c362ea93
foxido <git@forestfox.tk>
2022-12-16 00:50:30 +0300

    Add middleend to run script

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -2,7 +2,8 @@
 
 mkdir -p /tmp/examples
 
-./bin/front $1 /tmp/$1.ast                      && \
-    ./bin/back  /tmp/$1.ast /tmp/$1.asm         && \
-    ./cpu/bin/asm /tmp/$1.asm /tmp/$1.bin       && \
-    ./cpu/bin/cpu /tmp/$1.bin
+./bin/front        $1           /tmp/$1.ast         && \
+./bin/middle  /tmp/$1.ast       /tmp/$1.opt.ast     && \
+./bin/back    /tmp/$1.opt.ast   /tmp/$1.asm         && \
+./cpu/bin/asm /tmp/$1.asm       /tmp/$1.bin         && \
+./cpu/bin/cpu /tmp/$1.bin
